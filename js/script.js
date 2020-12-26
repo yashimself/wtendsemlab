08a08dcffd104e82755adabd5897cf640a23d8ff
Yash <yashshirbhate@pm.me>
2020-12-26 13:10:27 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function check(){
     var str = document.getElementById('inpt').value;
     str = str.toLowerCase();
+    str = str.split(" ");
     const len = str.length;
     var i;
     for (i = 0; i<len/2; i++) {
